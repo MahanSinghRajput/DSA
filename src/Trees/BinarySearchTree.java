525a2850832232f25684c5939bfb2a7b15da14f2
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2025-11-05 15:31:34 +0530

    insert function written

--- src/Trees/BinarySearchTree.java
+++ src/Trees/BinarySearchTree.java
@@ -42,5 +42,18 @@ public class BinarySearchTree {
     public void insert(int value){
         
     }
-
+    private Node insert(int value, Node node){
+        if(node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.value){
+            node.left = insert(value,node.left);
+        }
+        if(value > node.value){
+            node.right = insert(value,node.right);
+        }
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        return node;
+    }
 }
